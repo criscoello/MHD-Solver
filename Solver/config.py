4370290c5c015bca85dbfeb8ae59c0279799ba46
Cristhian Coello <skylordmanuel@gmail.com>
2025-10-12 23:00:02 +0200

    Initialization of parameters

---
+++ Solver/config.py
@@ -0,0 +1,30 @@
+#Initialization of the grid
+
+#Number of Points
+Nx = 128
+Ny = 128
+
+#Length of the Grid
+Lx = 1.0
+Ly = 0.5
+
+#Cell Dimension
+dx = Lx/Nx
+dy = Ly/Ny
+
+#Initialization of the Physical Constants
+
+#Adiabatic Factor
+gamma = 5/3 #monoatomic
+
+#Resistivity
+eta = 1e-3
+
+#Time control parameters
+cfl = 0.3
+output_interval = 50
+tmax = 1.0
+
+#For Safety(Optional)
+min_density = 1e-4
+min_pressure = 1e-6
